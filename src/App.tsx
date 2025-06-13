213d1ad76affd27981c18e6618f8e79ca2f0484a
Dario George <edu.dariogeorge21@gmail.com>
2025-06-13 23:46:24 +0530

    divs added

--- src/App.tsx
+++ src/App.tsx
@@ -20,38 +20,260 @@ function App() {
   const [errorMessage,setErrorMessage] = useState('');
   const [pokemonStats, setPokemonStats] = useState<(pokemonStatsType) | null>(null);
 
+  const 
+
   return (
     <>
       <header className='heading'>Pokemon Benchmark</header>
       <br /><hr className='hr'/>
       <h2 className='subHead'>Benchmark Area</h2>
       <div className="container">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
       </div>
       <br /><hr className='hr'/>
       <h2 className='subHead'>Pokemon Arena</h2>
       <div className="container">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
+        <div className="card">
+          <div className="name"></div>
+          <div className="height"></div>
+          <div className="weight"></div>
+          <div className="hp"></div>
+          <div className="attack"></div>
+          <div className="defense"></div>
+          <div className="specialAttack"></div>
+          <div className="specialDefense"></div>
+          <div className="speed"></div>
+        </div>
       </div>
     </>
   )
