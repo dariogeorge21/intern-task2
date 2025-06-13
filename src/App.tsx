df08c054590cf2ef6a53eb6e7d95b7641d6ac697
Dario George <edu.dariogeorge21@gmail.com>
2025-06-13 14:39:03 +0530

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,36 @@ function App() {
   return (
     <>
       <header className='heading'>Pokemon Benchmark</header>
+      <br /><hr className='hr'/>
+      <h2 className='subHead'>Benchmark Area</h2>
+      <div className="container">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+      <br /><hr className='hr'/>
+      <h2 className='subHead'>Pokemon Arena</h2>
+      <div className="container">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
     </>
   )
 }
